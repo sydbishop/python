7bb08bc867b38ea886bde8be8c586b58fe03c148
sydbishop <137097593+sydbishop@users.noreply.github.com>
2023-06-19 12:56:17 -0400

    Add files via upload
    Assignments 1 & 2

---
+++ Bishop_Lab1.py
@@ -0,0 +1,43 @@
+# Program Name: Bishop_Lab1.py
+# Course: IT1113/Section W01
+# Student Name: Sydney Bishop
+# Assignment Number: Lab 1
+# Due Date: 06/12/2023
+# Purpose: Grocery store prompt - input and output of variables
+
+# create variables with user input
+item_1 = input("enter the name of the first item: ")
+item_1_cost =  input("enter the price for the item: ")
+
+item_2 = input("enter the name of the next item: ")
+item_2_cost = input("enter the price for the item: ")
+
+item_3 = input("enter the name of the next item: ")
+item_3_cost = input("enter the price for the item: ")
+
+item_4 = input("enter the name of the next item: ")
+item_4_cost = input("enter the price for the item: ")
+
+item_5 = input("enter the name of the next item: ")
+item_5_cost = input("enter the price for the item: ")
+
+item_6 = input("enter the name of the next item: ")
+item_6_cost = input("enter the price for the item: ")
+
+item_7 = input("enter the name of the next item: ")
+item_7_cost = input("enter the price for the item: ")
+
+item_8 = input("enter the name of the last item: ")
+item_8_cost = input("enter the price for the item: ")
+
+# print grocery list
+print("\t")
+print("Final Grocery List: ")
+print(item_1, "\t\t", item_1_cost)
+print(item_2, "\t", item_2_cost)
+print(item_3, "\t\t", item_3_cost)
+print(item_4, "\t\t", item_4_cost)
+print(item_5, "\t\t", item_5_cost)
+print(item_6, "\t", item_6_cost)
+print(item_7, "\t", item_7_cost)
+print(item_8, "\t\t", item_8_cost)
---
+++ Bishop_Lab2.py
@@ -0,0 +1,321 @@
+# Program Name: Bishop_Lab2.py
+# Course: IT1113/Section W01
+# Student Name: Sydney Bishop
+# Assignment Number: Lab 2
+# Due Date: 06/18/2023
+# Purpose: if-else and if-elif-else statements
+
+# create variables with user input
+name_1 = input("Enter your first and last name: ")
+books_1 = int(input("Enter the number of books you have purchased this month: "))
+
+# assign points based on number of books purchased
+if books_1 >= 1: 
+    if books_1 >= 3:
+        if books_1 >= 5:
+            if books_1 >= 6:
+              if books_1 >= 8:
+                points = int(25)
+                print (name_1,' earned ', points, ' points this month.')            
+              else:
+                  points = int(20)
+                  print (name_1,' earned ', points, ' points this month.') 
+            else:
+                points = int(15)
+                print (name_1,' earned ', points, ' points this month.') 
+        else:
+            points = int(10)
+            print (name_1,' earned ', points, ' points this month.') 
+    else:
+        points = int(5)
+        print (name_1,' earned ', points, ' points this month.') 
+else:
+    points = int(0)
+    print (name_1,' earned ', points, ' points this month.') 
+
+# save variable for bookstore total
+points_1 = points
+
+# repeat process for remaining customers
+# create variables with user input
+name_2 = input("Enter your first and last name: ")
+books_2 = int(input("Enter the number of books you have purchased this month: "))
+
+# assign points based on number of books purchased
+if books_2 >= 1: 
+    if books_2 >= 3:
+        if books_2 >= 5:
+            if books_2 >= 6:
+              if books_2 >= 8:
+                points = int(25)
+                print (name_2,' earned ', points, ' points this month.')            
+              else:
+                  points = int(20)
+                  print (name_2,' earned ', points, ' points this month.')   
+            else:
+                points = int(15)
+                print (name_2,' earned ', points, ' points this month.')   
+        else:
+            points = int(10)
+            print (name_2,' earned ', points, ' points this month.')   
+    else:
+        points = int(5)
+        print (name_2,' earned ', points, ' points this month.')   
+else:
+    points = int(0)
+    print (name_2,' earned ', points, ' points this month.')   
+
+# save variable for bookstore total
+points_2 = points
+
+# create variables with user input
+name_3 = input("Enter your first and last name: ")
+books_3 = int(input("Enter the number of books you have purchased this month: "))
+
+# assign points based on number of books purchased
+if books_3 >= 1: 
+    if books_3 >= 3:
+        if books_3 >= 5:
+            if books_3 >= 6:
+              if books_3 >= 8:
+                points = int(25)
+                print (name_3,' earned ', points, ' points this month.')              
+              else:
+                  points = int(20)
+                  print (name_3,' earned ', points, ' points this month.') 
+            else:
+                points = int(15)
+                print (name_3,' earned ', points, ' points this month.') 
+        else:
+            points = int(10)
+            print (name_3,' earned ', points, ' points this month.') 
+    else:
+        points = int(5)
+        print (name_3,' earned ', points, ' points this month.') 
+else:
+    points = int(0)
+    print (name_3,' earned ', points, ' points this month.') 
+
+# save variable for bookstore total
+points_3 = points
+
+# create variables with user input
+name_4 = input("Enter your first and last name: ")
+books_4 = int(input("Enter the number of books you have purchased this month: "))
+
+# assign points based on number of books purchased
+if books_4 >= 1: 
+    if books_4 >= 3:
+        if books_4 >= 5:
+            if books_4 >= 6:
+              if books_4 >= 8:
+                points = int(25)
+                print (name_4,' earned ', points, ' points this month.')         
+              else:
+                  points = int(20)
+                  print (name_4,' earned ', points, ' points this month.') 
+            else:
+                points = int(15)
+                print (name_4,' earned ', points, ' points this month.') 
+        else:
+            points = int(10)
+            print (name_4,' earned ', points, ' points this month.') 
+    else:
+        points = int(5)
+        print (name_4,' earned ', points, ' points this month.') 
+else:
+    points = int(0)
+    print (name_4,' earned ', points, ' points this month.') 
+
+# save variable for bookstore total
+points_4 = points
+
+# create variables with user input
+name_5 = input("Enter your first and last name: ")
+books_5 = int(input("Enter the number of books you have purchased this month: "))
+
+# assign points based on number of books purchased
+if books_5 >= 1: 
+    if books_5 >= 3:
+        if books_5 >= 5:
+            if books_5 >= 6:
+              if books_5 >= 8:
+                points = int(25)
+                print (name_5,' earned ', points, ' points this month.')         
+              else:
+                  points = int(20)
+                  print (name_5,' earned ', points, ' points this month.')    
+            else:
+                points = int(15)
+                print (name_5,' earned ', points, ' points this month.')    
+        else:
+            points = int(10)
+            print (name_5,' earned ', points, ' points this month.')    
+    else:
+        points = int(5)
+        print (name_5,' earned ', points, ' points this month.')    
+else:
+    points = int(0)
+    print (name_5,' earned ', points, ' points this month.')    
+
+# save variable for bookstore total
+points_5 = points
+
+# create variables with user input
+name_6 = input("Enter your first and last name: ")
+books_6 = int(input("Enter the number of books you have purchased this month: "))
+
+# assign points based on number of books purchased
+if books_6 >= 1: 
+    if books_6 >= 3:
+        if books_6 >= 5:
+            if books_6 >= 6:
+              if books_6 >= 8:
+                points = int(25)
+                print (name_6,' earned ', points, ' points this month.')            
+              else:
+                  points = int(20)
+                  print (name_6,' earned ', points, ' points this month.')  
+            else:
+                points = int(15)
+                print (name_6,' earned ', points, ' points this month.')  
+        else:
+            points = int(10)
+            print (name_6,' earned ', points, ' points this month.')  
+    else:
+        points = int(5)
+        print (name_6,' earned ', points, ' points this month.')  
+else:
+    points = int(0)
+    print (name_6,' earned ', points, ' points this month.')  
+
+# save variable for bookstore total
+points_6 = points
+
+# create variables with user input
+name_7 = input("Enter your first and last name: ")
+books_7 = int(input("Enter the number of books you have purchased this month: "))
+
+# assign points based on number of books purchased
+if books_7 >= 1: 
+    if books_7 >= 3:
+        if books_7 >= 5:
+            if books_7 >= 6:
+              if books_7 >= 8:
+                points = int(25)
+                print (name_7,' earned ', points, ' points this month.')             
+              else:
+                  points = int(20)
+                  print (name_7,' earned ', points, ' points this month.')  
+            else:
+                points = int(15)
+                print (name_7,' earned ', points, ' points this month.')  
+        else:
+            points = int(10)
+            print (name_7,' earned ', points, ' points this month.')  
+    else:
+        points = int(5)
+        print (name_7,' earned ', points, ' points this month.')  
+else:
+    points = int(0)
+    print (name_7,' earned ', points, ' points this month.')  
+
+# save variable for bookstore total
+points_7 = points
+
+# create variables with user input
+name_8 = input("Enter your first and last name: ")
+books_8 = int(input("Enter the number of books you have purchased this month: "))
+
+# assign points based on number of books purchased
+if books_8 >= 1: 
+    if books_8 >= 3:
+        if books_8 >= 5:
+            if books_8 >= 6:
+              if books_8 >= 8:
+                points = int(25)
+                print (name_8,' earned ', points, ' points this month.')            
+              else:
+                  points = int(20)
+                  print (name_8,' earned ', points, ' points this month.') 
+            else:
+                points = int(15)
+                print (name_8,' earned ', points, ' points this month.') 
+        else:
+            points = int(10)
+            print (name_8,' earned ', points, ' points this month.') 
+    else:
+        points = int(5)
+        print (name_8,' earned ', points, ' points this month.') 
+else:
+    points = int(0)
+    print (name_8,' earned ', points, ' points this month.') 
+
+# save variable for bookstore total
+points_8 = points
+
+# create variables with user input
+name_9 = input("Enter your first and last name: ")
+books_9 = int(input("Enter the number of books you have purchased this month: "))
+
+# assign points based on number of books purchased
+if books_9 >= 1: 
+    if books_9 >= 3:
+        if books_9 >= 5:
+            if books_9 >= 6:
+              if books_9 >= 8:
+                points = int(25)
+                print (name_9,' earned ', points, ' points this month.')          
+              else:
+                  points = int(20)
+                  print (name_9,' earned ', points, ' points this month.')  
+            else:
+                points = int(15)
+                print (name_9,' earned ', points, ' points this month.')  
+        else:
+            points = int(10)
+            print (name_9,' earned ', points, ' points this month.')  
+    else:
+        points = int(5)
+        print (name_9,' earned ', points, ' points this month.')  
+else:
+    points = int(0)
+    print (name_9,' earned ', points, ' points this month.')  
+
+# save variable for bookstore total
+points_9 = points
+
+# create variables with user input
+name_10 = input("Enter your first and last name: ")
+books_10 = int(input("Enter the number of books you have purchased this month: "))
+
+# assign points based on number of books purchased
+if books_10 >= 1: 
+    if books_10 >= 3:
+        if books_10 >= 5:
+            if books_10 >= 6:
+              if books_10 >= 8:
+                points = int(25)
+                print (name_10,' earned ', points, ' points this month.')             
+              else:
+                  points = int(20)
+                  print (name_10,' earned ', points, ' points this month.')   
+            else:
+                points = int(15)
+                print (name_10,' earned ', points, ' points this month.')   
+        else:
+            points = int(10)
+            print (name_10,' earned ', points, ' points this month.')   
+    else:
+        points = int(5)
+        print (name_10,' earned ', points, ' points this month.')   
+else:
+    points = int(0)
+    print (name_10,' earned ', points, ' points this month.')   
+
+# save variable for bookstore total
+points_10 = points
+
+# add customer points for bookstore total
+total_points = points_1 + points_2 + points_3 + points_4 + points_5 + points_6 + points_7 + points_8 + points_9 + points_10
+print('The bookstore has earned a total of ', total_points, ' points this month.')
